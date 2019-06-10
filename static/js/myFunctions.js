9a7441889c82bc56eed0a3721580900b7d9d980c
Darshan Baral <darshanbaral@gmail.com>
2019-06-09 17:39:40 -0700

    Converted menu to hover type

--- static/js/myFunctions.js
+++ static/js/myFunctions.js
@@ -1,35 +1,3 @@
-let showBooks = function() {
-  let options = document.querySelector(".chapters");
-  let menuItems = document.querySelector(".menuItems");
-  let mainItem = document.querySelector("main");
-  if (!options.classList.contains("d-none")) {
-    options.classList.add("d-none");
-  }
-  if (menuItems.classList.contains("d-none")) {
-    menuItems.classList.remove("d-none");
-    mainItem.classList.add("d-none");
-  } else {
-    menuItems.classList.add("d-none");
-    mainItem.classList.remove("d-none");
-  }
-};
-
-let showChapters = function() {
-  let menuItems = document.querySelector(".menuItems");
-  let options = document.querySelector(".chapters");
-  let mainItem = document.querySelector("main");
-  if (!menuItems.classList.contains("d-none")) {
-    menuItems.classList.add("d-none");
-  }
-  if (options.classList.contains("d-none")) {
-    options.classList.remove("d-none");
-    mainItem.classList.add("d-none");
-  } else {
-    options.classList.add("d-none");
-    mainItem.classList.remove("d-none");
-  }
-};
-
 let scrollToTop = () => {
   const c = document.documentElement.scrollTop || document.body.scrollTop;
   if (c > 0) {
