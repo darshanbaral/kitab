d89dd25b52caa3e60ca68e366e6c765835d9c130
Darshan Baral <darshanbaral@gmail.com>
2019-06-17 19:38:52 -0700

    Using clipboard.js to allow copying in iOS

--- static/js/myFunctions.js
+++ static/js/myFunctions.js
@@ -30,26 +30,3 @@ let scrollToTop = () => {
     window.scrollTo(0, c - c / 8);
   }
 };
-
-function copyUrl(text) {
-  let copyFrom = document.createElement("textarea");
-  copyFrom.textContent = text;
-  document.body.appendChild(copyFrom);
-  copyFrom.select();
-  document.execCommand("copy");
-  copyFrom.blur();
-  document.body.removeChild(copyFrom);
-
-  let copiedMessage = document.createElement("div");
-  copiedMessage.id = "copiedMessage";
-  copiedMessage.textContent = "Link copied to clipboard";
-  copiedMessage.classList.add("bg-warning", "shadow", "d-block", "text-center");
-  document.body.appendChild(copiedMessage);
-
-  setTimeout(function() {
-    copiedMessage.style.opacity = 0;
-  }, 500);
-  setTimeout(function() {
-    document.body.removeChild(copiedMessage);
-  }, 1500);
-}
