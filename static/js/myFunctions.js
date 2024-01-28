92cfd604b846eeb020d9e714836c33bce6623f8a
Darshan Baral <darshanbaral@gmail.com>
2024-01-28 15:12:33 -0800

    removed fa, updated bootstrap

--- static/js/myFunctions.js
+++ static/js/myFunctions.js
@@ -1,27 +1,27 @@
-window.onclick = function (event) {
-  checkClick(event);
-};
-window.ontouchstart = function (event) {
-  checkClick(event);
-};
+// window.onclick = function (event) {
+//   checkClick(event);
+// };
+// window.ontouchstart = function (event) {
+//   checkClick(event);
+// };
 
-checkClick = function (event) {
-  let insideMenu = false;
-  Array.from(document.querySelectorAll(".show")).forEach((menu) => {
-    insideMenu = insideMenu || menu.contains(event.target);
-  });
-  if (!insideMenu && !event.target.matches(".hover-menu")) {
-    Array.from(document.querySelectorAll(".hover-menu")).forEach(
-      (hovermenu) => {
-        hovermenu.nextElementSibling.style.maxHeight = "0px";
-      }
-    );
-  }
-};
-let expandMenu = function (x) {
-  Array.from(document.querySelectorAll(".hover-menu")).forEach((hovermenu) => {
-    hovermenu.nextElementSibling.style.maxHeight = "0px";
-  });
-  let thisMenu = x.nextElementSibling;
-  thisMenu.style.maxHeight = thisMenu.scrollHeight + "px";
-};
+// checkClick = function (event) {
+//   let insideMenu = false;
+//   Array.from(document.querySelectorAll(".show")).forEach((menu) => {
+//     insideMenu = insideMenu || menu.contains(event.target);
+//   });
+//   if (!insideMenu && !event.target.matches(".hover-menu")) {
+//     Array.from(document.querySelectorAll(".hover-menu")).forEach(
+//       (hovermenu) => {
+//         hovermenu.nextElementSibling.style.maxHeight = "0px";
+//       }
+//     );
+//   }
+// };
+// let expandMenu = function (x) {
+//   Array.from(document.querySelectorAll(".hover-menu")).forEach((hovermenu) => {
+//     hovermenu.nextElementSibling.style.maxHeight = "0px";
+//   });
+//   let thisMenu = x.nextElementSibling;
+//   thisMenu.style.maxHeight = thisMenu.scrollHeight + "px";
+// };
